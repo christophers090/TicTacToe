7544cc23940e66966ace94231a910a30eb6c236b
Chris Sheehan <christopher.sheehan.28@gmail.com>
2023-11-03 20:03:15 -0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Chris Sheehan on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
